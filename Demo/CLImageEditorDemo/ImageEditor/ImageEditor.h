9442cc7c0b7d4b514070e7b989c975379e5fd40b
Danish <danish.aziz@photobookworldwide.com>
2018-03-28 16:49:25 +0800

    add new scheme

---
+++ Demo/CLImageEditorDemo/ImageEditor/ImageEditor.h
@@ -0,0 +1,19 @@
+//
+//  ImageEditor.h
+//  ImageEditor
+//
+//  Created by Danish on 28/03/2018.
+//  Copyright © 2018 CALACULU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImageEditor.
+FOUNDATION_EXPORT double ImageEditorVersionNumber;
+
+//! Project version string for ImageEditor.
+FOUNDATION_EXPORT const unsigned char ImageEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImageEditor/PublicHeader.h>
+
+
