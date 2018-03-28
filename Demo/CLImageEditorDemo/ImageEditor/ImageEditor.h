dff756ad568bdf0c05402d4a5354ad2c1ed8336e
Danish <danish.aziz@photobookworldwide.com>
2018-03-28 18:28:37 +0800

    update schema

--- Demo/CLImageEditorDemo/ImageEditor/ImageEditor.h
+++ Demo/CLImageEditorDemo/ImageEditor/ImageEditor.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char ImageEditorVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ImageEditor/PublicHeader.h>
 
-
+#import <ImageEditor/CLImageEditor.h>
