477932e457ab2c6f34382b589d3dac8b88b23347
Danish <danish.aziz@photobookworldwide.com>
2018-10-24 14:53:54 +0800

    change calculation

--- CLImageEditor/ViewController/_CLImageEditorViewController.m
+++ CLImageEditor/ViewController/_CLImageEditorViewController.m
@@ -1140,14 +1140,14 @@ static const CGFloat kMenuBarHeight = 80.0f;
                 rct.origin.y    *= zoomScale;
                 if (!self.isCropingFirstTime) {
 
-                    rct.size.width += ((self.bleedAreaRightByPercentage * zoomScale) / 1);
-                    rct.size.width += ((self.bleedAreaLeftByPercentage * zoomScale) / 1);
+                    rct.size.width += (self.bleedAreaRightByPercentage / 2);
+                    rct.size.width += (self.bleedAreaLeftByPercentage / 2);
 
-                    rct.size.height += ((self.bleedAreaTopByPercentage * zoomScale) / 1);
-                    rct.size.height += ((self.bleedAreaBottomByPercentage * zoomScale) / 1);
+                    rct.size.height += (self.bleedAreaTopByPercentage / 2);
+                    rct.size.height += (self.bleedAreaBottomByPercentage / 2);
                     
-                    rct.origin.x -= ((self.bleedAreaLeftByPercentage * zoomScale) / 1);
-                    rct.origin.y -= ((self.bleedAreaTopByPercentage * zoomScale) / 1);
+                    rct.origin.x -= (self.bleedAreaLeftByPercentage / 2);
+                    rct.origin.y -= (self.bleedAreaTopByPercentage / 2);
 
                 }
                 rct.size.width  /= zoomScale;
